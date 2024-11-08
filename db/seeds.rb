c2d7c96c10b9446f5f19ec7bd97c698358640f79
Alora Riley <alora.riley@gmail.com>
2024-11-08 15:48:34 -0500

    Save coupon data in the comments for rails c later use

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,12 @@ puts "Loading PostgreSQL Data dump into local database with command:"
 puts cmd
 system(cmd)
 
+# Coupon.create! name:"Buy One Get One 50% off", code:"BOGO50", discount_type:"percent", discount_value:25.00, merchant_id:5
+# Coupon.create! name:"Flash Sale", code:"FLASH40NOW", discount_type:"percent", discount_value:40.00, merchant_id:5
+# Coupon.create! name:"Sign Up With Your Email", code:"EMAIL20", discount_type:"percent", discount_value:20.00, merchant_id:8
+# Coupon.create! name:"Grand Oppening", code:"TEN4EVERY1", discount_type:"dollar", discount_value:10.00, merchant_id:8
+# Coupon.create! name: "Summer Clearance", code: "SUMMER25", discount_type: "percent", discount_value: 25.00, merchant_id: 3
+# Coupon.create! name: "First Time Customer", code: "WELCOME15", discount_type: "percent", discount_value: 15.00, merchant_id: 7
+# Coupon.create! name: "Holiday Special", code: "HOLIDAY50", discount_type: "dollar", discount_value: 50.00, merchant_id: 2
+# Coupon.create! name: "Loyalty Reward", code: "LOYAL10", discount_type: "dollar", discount_value: 15.00, merchant_id: 6
+# Coupon.create! name: "Free Shipping", code: "SHIPFREE", discount_type: "dollar", discount_value: 7.00, merchant_id: 4
