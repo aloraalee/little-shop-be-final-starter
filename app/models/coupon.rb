5d8c44f1efa2f30417ba406dcd5fcf970c7c926b
Alora Riley <alora.riley@gmail.com>
2024-11-09 17:03:26 -0500

    test: Move validations to the coupon model test

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,9 +1,13 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
   has_many :invoices
-
-  validates_presence_of :name, :code, :discount_type, :discount_value, :active, :merchant_id
-  validates :discount_value, numericality: {greater_then_or_equal_to: 0}
+  validates_presence_of :name 
+  validates_presence_of :code 
+  validates_presence_of :discount_type 
+  validates_presence_of :discount_value 
+  validates_presence_of :active
+  validates_presence_of :merchant_id
+  validates :discount_value, numericality: { greater_than_or_equal_to: 0 }
   validates :code, uniqueness: { case_sensitive: false }
   # validate :merchant_active_coupon_limit
 
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -2,7 +2,13 @@ require "rails_helper"
 
 describe Coupon, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:code) }
+    it { should validate_presence_of(:discount_type)}
+    it { should validate_presence_of(:discount_value)}
+    it { should validate_presence_of(:active)}
+    it { should validate_presence_of(:merchant_id)}
+    it { should validate_numericality_of(:discount_value).is_greater_than_or_equal_to(0)}
   end
 
   describe 'relationships' do
