d6b6afbffbd33e86a6c32ae129009b3ebd133aa5
Alora Riley <alora.riley@gmail.com>
2024-11-09 20:26:58 -0500

    test: Add test to test merchant_active_coupon_limit

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -6,17 +6,15 @@ class Coupon < ApplicationRecord
   validates_presence_of :discount_type 
   validates_presence_of :discount_value 
   validates_presence_of :active
-  validates_presence_of :merchant_id
   validates :discount_value, numericality: { greater_than_or_equal_to: 0 }
   validates :code, uniqueness: { case_sensitive: false }
-  # validate :merchant_active_coupon_limit
+  validate :merchant_active_coupon_limit
 
   private
 
   def merchant_active_coupon_limit
-    if merchant.coupons.where(active: true).count >= 5 && active?
+    if merchant && active? && merchant.coupons.where(active: true).count >= 5
       errors.add(:base, "This merchant has reached the 5 active coupon limit")
     end
   end
-
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -7,12 +7,28 @@ describe Coupon, type: :model do
     it { should validate_presence_of(:discount_type)}
     it { should validate_presence_of(:discount_value)}
     it { should validate_presence_of(:active)}
-    it { should validate_presence_of(:merchant_id)}
     it { should validate_numericality_of(:discount_value).is_greater_than_or_equal_to(0)}
   end
 
   describe 'relationships' do
     it { should belong_to :merchant }
     it { should have_many :invoices }
+  end
+
+  describe "Merchant coupons endpoints" do
+    before :each do
+      @merchant1 = create(:merchant)
+      @coupon1 = create(:coupon, merchant_id: @merchant1.id)
+      @coupon2 = create(:coupon, merchant_id: @merchant1.id)
+      @coupon3 = create(:coupon, merchant_id: @merchant1.id)
+      @coupon4 = create(:coupon, merchant_id: @merchant1.id)
+      @coupon5 = create(:coupon, merchant_id: @merchant1.id)
+    end
+    
+    it "returns an error if you try to add more than five coupons to a merchant" do
+      coupon6 = build(:coupon, merchant_id: @merchant1.id)
+
+      expect(coupon6).not_to be_valid
+    end
   end
 end
